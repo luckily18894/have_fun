3b3fa10d46a57594e4c832e5a66053711e6aa014
luckily18894 <185686792@qq.com>
2019-09-30 10:18:13 +0800

    ssh 登录 敲命令 保存回显至txt （已解决----More----分页问题 及 空行问题）

--- ssh_sth.py
+++ ssh_sth.py
@@ -1,6 +1,7 @@
 # -*- coding=utf-8 -*-
 
 import paramiko
+import datetime
 import time
 import re
 
@@ -14,7 +15,7 @@ def ssh_cmd(ip, username, password, cmd_list, verbose=True):
 
     chan = ssh.invoke_shell()  # 激活交互式shell
     time.sleep(2)
-    x = chan.recv(2048).decode()  # 接收回显信息
+    # x = chan.recv(2048).decode()  # 接收回显信息
     resul = ''
     space = ' '  # 要发送的空格
 
@@ -24,39 +25,43 @@ def ssh_cmd(ip, username, password, cmd_list, verbose=True):
     time.sleep(2)  # 由于有些回显可能过长，所以可以考虑等待更长一些时间
     x = chan.recv(40960).decode()  # 读取回显，有些回显可能过长，请把接收缓存调大
 
-    abc = re.sub('\s+----\sMore\s----', '', x)
-    resul += abc
+    # o = re.sub('\s+----\sMore\s----', '', x)
+    o = re.sub('\r\n', '\n', re.sub('\s+----\sMore\s----', '', x), re.S | re.M)
+    resul += re.sub('\r\n', '\n', o, re.S | re.M)
 
     # 一直空格 直到回显完
     while 1:
         # 如果 本次回显为 <sysname> 就退出循环 否则 继续发送空格 读取命令剩余的回显
             if re.match('^<.+>', x.strip()):  # strip 很重要！！！
+                sysname = re.match('^<(.+)>', x.strip()).groups()[0]
                 break
             else:
                 chan.send(space.encode())
                 chan.send(b'\n')
                 time.sleep(1)
                 x = chan.recv(40960).decode()
-                abc = re.sub('\s+----\sMore\s----', '', x)
-                resul += re.sub('\x1b\[\d{2}D[\s]+\x1b\[\d{2}D', '', abc)
+                # abc = re.sub('\s+----\sMore\s----', '', x)
+                o = re.sub('\x1b\[\d{2}D[\s]+\x1b\[\d{2}D', '\r\n', re.sub('\s+----\sMore\s----', '', x))
                 # \x1b[42D                                          \x1b[42D
+                resul += re.sub('\r\n', '\n', o, re.S | re.M)
                 # 空格后回显会有上面这类字符，需要干掉
 
     chan.close()  # 退出交互式shell
     ssh.close()  # 退出ssh会话
-
-    return resul
+    print([resul])
+    return resul, sysname
 
 
 def resule_to_txt(ipadd, cmd):
-    res = ssh_cmd(ipadd, 'wujiajie', '1ffw#1*dF', cmd)
-    with open('{} - {}.txt'.format(ipadd, cmd), 'w') as f:
+    res, sysname = ssh_cmd(ipadd, 'wujiajie', '1ffw#1*dF', cmd)
+    ti = datetime.datetime.now().strftime('%Y.%m.%d %H.%M.%S')
+    with open('{} -- {} -- {}.txt'.format(sysname, ipadd, ti), 'w') as f:
         f.write(res)
-    print('{} - {}'.format(ipadd, cmd) + '  已完成！')
+    print('{} -- {} -- {}'.format(sysname, ipadd, ti) + '  已完成！')
 
 
 if __name__ == '__main__':
     # print(ssh_cmd('192.168.0.97', 'wujiajie', '1ffw#1*dF', 'dis cu'))
-    resule_to_txt('218.205.112.23', 'dis cu')
+    resule_to_txt('218.205.90.52', 'dis cu')
 
 
